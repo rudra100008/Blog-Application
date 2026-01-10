9566ce63caba9ed20460cf4773d1275833b74fdf
rudra100008 <ashumbhuju123@gmail.com>
2026-01-10 08:48:37 +0545

    Blog Frontend code

--- blog/src/app/hooks/useCategory.js
+++ blog/src/app/hooks/useCategory.js
@@ -1,24 +1,25 @@
-import { useEffect, useState } from "react"
+import { useEffect, useState } from "react";
 import { fetchAllCategories } from "../services/CategoryServices";
 
-export const useCategory = ()=>{
-    const [categories,setCategories] = useState([]);
+export const useCategory = () => {
+  const [categories, setCategories] = useState([]);
+  const setAllCategories = async () => {
+    if (typeof window === 'undefined') return;
     const token = localStorage.getItem("token");
-    const setAllCategories  = async()=>{
-        try{
-            const data = await fetchAllCategories(token);
-            setCategories(data);
-        }catch(err){
-            console.error("Error in setAllCategories: ",err.response?.data)
-        }
+    try {
+      const data = await fetchAllCategories(token);
+      setCategories(data);
+    } catch (err) {
+      console.error("Error in setAllCategories: ", err.response?.data);
     }
+  };
 
-    useEffect(()=>{
-        setAllCategories();
-    },[])
-    return {
-        categories,
-        setCategories,
-        setAllCategories,
-    }
-}
+  useEffect(() => {
+    setAllCategories();
+  }, []);
+  return {
+    categories,
+    setCategories,
+    setAllCategories,
+  };
+};
