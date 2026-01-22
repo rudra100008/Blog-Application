55f13915ac8b0ec973396cecb0091ffb000622e6
rudra100008 <ashumbhuju123@gmail.com>
2026-01-22 15:03:50 +0545

    Backend code

--- blogrestapi/src/main/java/com/blogrestapi/Security/SecurityConfig.java
+++ blogrestapi/src/main/java/com/blogrestapi/Security/SecurityConfig.java
@@ -98,7 +98,8 @@ public class SecurityConfig {
         List<String> allowedOrigins = List.of(
                 "http://localhost:3000",
                 "https://blog-application-jmzl.vercel.app",
-                "https://blog-application-jmzl-d7ev5c4u5-ashum123s-projects.vercel.app/"
+                "https://blog-application-jmzl-d7ev5c4u5-ashum123s-projects.vercel.app/",
+                "https://blog-application-jmzl-git-vercel-deploy-ashum123s-projects.vercel.app/"
         );
 
         corsConfig.setAllowedOrigins(allowedOrigins);
